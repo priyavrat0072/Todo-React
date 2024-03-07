f1cfaedf3fd1ed5b75869893acde87db34fcac5d
priyavrat0072 <priyavrat0072@gmail.com>
2024-03-07 13:20:37 +0530

    Color

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -18,16 +18,18 @@ function TodoForm() {
     return (
         <form onSubmit={add} className="flex">
             <input
-                
+                 className=" text-blue-500 w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150  bg-white/20 py-1.5"
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+               
                 value={todo}
                 onChange={(e) => setTodo(e.target.value)}
             />
-            <button type="submit" className="rounded-r-lg px-3 py-1 bg-yellow-600 text-white-shrink-0">
+            <button type="submit" className="rounded-r-lg px-3 py-1 text-white-shrink-0 ">
                 Add
             </button>
+
+            
         </form>
     )
 }
